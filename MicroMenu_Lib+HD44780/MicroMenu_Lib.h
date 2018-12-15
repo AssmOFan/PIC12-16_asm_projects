867b307465ecbe67ad950ad93567043f80bc77e4
AssmOFan <bor1andrey@gmail.com>
2018-12-15 10:43:18 +0200

    Мелкие косметические изменения перед публикацией на Easyelectronics.ru Добавлено краткое описание библиотеки в MicroMenu_Lib.h

--- MicroMenu_Lib+HD44780/MicroMenu_Lib.h
+++ MicroMenu_Lib+HD44780/MicroMenu_Lib.h
@@ -1,16 +1,19 @@
 ; Микроменю на базе двухстрочного индикатора HD44780 и четырехпозиционного джойстика (крестовины кнопок)
+; Библиотека построена на основе "индексов". Меню имеет 2 уровня вложености (вход в пункт меню + вход в подпункт меню)
+; При навигации по меню изменяются номера ("индексы") текущего пункта меню и текущего подпункта меню
+; На основе этих данных и выводится информация на дисплей/совершаются действия
 
 include	hardware_profile.inc
 include	Macro.h	
 
-#define	USE_CURSOR				0				; Всегда рисовать курсор в конце 1-й строки
+#define	USE_STATIC_CURSOR		0				; Всегда рисовать курсор в конце 1-й строки
 #define	USE_MOVING_CURSOR		1				; Использовать перемещаемый курсор (повышает удобство использования меню и размер прошивки)
 #define	USE_TOP_LAST_CURSOR		1				; Использовать отображение "первый пункт всегда сверху" и "последний пункт всегда снизу" (повышает удобство использования меню и размер прошивки)
 #define	USE_RBIE				1				; Использовать прерывание по изминению состояния PORTB для обработки кнопок (позволяет использовать сон)
-#define	USE_MENU_ACTION			1				; Использовать возможность запуска действий при входе в пункт меню
+#define	USE_MENU_ACTION			0				; Использовать возможность запуска действий при входе в пункт меню (повышает размер прошивки)
 
 if	USE_MENU_ACTION	
-#define	actions_menu_flags		b'00000001'		; Выставленный бит - есть действие при входе в этот пункт меню (не забыть заполнить обработчик действия)
+#define	actions_menu_flags		b'00000000'		; Выставленный бит - есть действие при входе в этот пункт меню (не забыть заполнить обработчик действия !!!)
 endif
 
 #define	UP						PORTB,4
@@ -23,12 +26,14 @@ endif
 #define	Press_EXIT				flags,2
 #define	Press_ENTER				flags,3	
 
-#define	EXIT_DELAY				.20				; Количество опросов кнопок, до выхода в предыдущий уровень (* 0,8 сек)
+#define	EXIT_DELAY				.20				; Количество опросов кнопок, до автовыхода в предыдущий уровень (* 0,8 сек)
 
 #define	ZASTAVKA_FIRST_STRING	"  MicroMenuLib"
 #define	ZASTAVKA_SECOND_STRING	"  ver 1.0 beta"
 
-#define	NUM_OF_MAIN_MENU_PUNKTS	.5				; Количество пунктов меню, до 10 
+#define	ACTION_COMPLETE_STRING	"Action Complete!"
+
+#define	NUM_OF_MAIN_MENU_PUNKTS	.5				; Количество пунктов меню, до 8 (можно и больше, но действия при входе в меню доступны только для первых 8 пунктов) 
 
 #define	MENU_NAME_1				"1.Menu_1"
 #define	MENU_NAME_2				"2.Menu_2"
@@ -36,35 +41,35 @@ endif
 #define	MENU_NAME_4				"4.Menu_4"
 #define	MENU_NAME_5				"5.Menu_5"	
 
-#define	NUM_OF_SUBMENU_PUNKTS_1	.3				; Количество подпунктов каждого пункта меню, до 10
-
-#define	MENU_SUBMENU_NAME_1_1	"1.1.Garage"
-#define	MENU_SUBMENU_NAME_1_2	"1.2.Doors"
-#define	MENU_SUBMENU_NAME_1_3	"1.3.House"
+#define	NUM_OF_SUBMENU_PUNKTS_1	.3				; Количество подпунктов каждого пункта меню (в теории до 64, но больше 8 не проверялось, памяти в контроллере не хватило)
+												; Не забываем оформить обработчик для каждого действия !!!
+#define	MENU_SUBMENU_NAME_1_1	"1.1.SubMenu"
+#define	MENU_SUBMENU_NAME_1_2	"1.2.SubMenu"
+#define	MENU_SUBMENU_NAME_1_3	"1.3.SubMenu"
 
 #define	NUM_OF_SUBMENU_PUNKTS_2	.3
 
-#define	MENU_SUBMENU_NAME_2_1	"2.1.Garage"
-#define	MENU_SUBMENU_NAME_2_2	"2.2.Doors"
-#define	MENU_SUBMENU_NAME_2_3	"2.3.House"
+#define	MENU_SUBMENU_NAME_2_1	"2.1.SubMenu"
+#define	MENU_SUBMENU_NAME_2_2	"2.2.SubMenu"
+#define	MENU_SUBMENU_NAME_2_3	"2.3.SubMenu"
 				
 #define	NUM_OF_SUBMENU_PUNKTS_3	.3
 
-#define	MENU_SUBMENU_NAME_3_1	"3.1.Review"
-#define	MENU_SUBMENU_NAME_3_2	"3.2.Shut Off"
-#define	MENU_SUBMENU_NAME_3_3	"3.3.FAQ"
+#define	MENU_SUBMENU_NAME_3_1	"3.1.SubMenu"
+#define	MENU_SUBMENU_NAME_3_2	"3.2.SubMenu"
+#define	MENU_SUBMENU_NAME_3_3	"3.3.SubMenu"
 
 #define	NUM_OF_SUBMENU_PUNKTS_4	.3
 
-#define	MENU_SUBMENU_NAME_4_1	"4.1.Review"
-#define	MENU_SUBMENU_NAME_4_2	"4.2.Shut Off"
-#define	MENU_SUBMENU_NAME_4_3	"4.3.FAQ"
+#define	MENU_SUBMENU_NAME_4_1	"4.1.SubMenu"
+#define	MENU_SUBMENU_NAME_4_2	"4.2.SubMenu"
+#define	MENU_SUBMENU_NAME_4_3	"4.3.SubMenu"
 
 #define	NUM_OF_SUBMENU_PUNKTS_5	.3
 
-#define	MENU_SUBMENU_NAME_5_1	"5.1.Config"
-#define	MENU_SUBMENU_NAME_5_2	"5.2.Settings"
-#define	MENU_SUBMENU_NAME_5_3	"5.3.About"
+#define	MENU_SUBMENU_NAME_5_1	"5.1.SubMenu"
+#define	MENU_SUBMENU_NAME_5_2	"5.2.SubMenu"
+#define	MENU_SUBMENU_NAME_5_3	"5.3.SubMenu"
 
 ;================================================================================================================
 Create_Menu_Names	macro				; Макрос, создает имена всех пунктов меню
@@ -95,7 +100,7 @@ goto_Punkt_Menu	macro					; Макрос, создает переходы на все пункты меню
 				endw
 				endm
 ;================================================================================================================
-goto_Punkt_Submenu	macro				; Макрос, создает переходы на все подпункты меню
+goto_Punkt_Submenu	macro				; Макрос, создает переходы на имена всех подпунктов меню
 					mod_PCL	index_menu				
 					local	c=1
 					while	c<NUM_OF_MAIN_MENU_PUNKTS+1
@@ -138,84 +143,123 @@ goto_Punkt_Submenu_action	macro		; Макрос, создает переходы на действия всех под
 							endm
 ;================================================================================================================
 Create_Submenu_Names	macro				; Макрос, создает имена всех подпунктов меню
-				local	d=1
+; К сожаления, почему-то не получилось создавать имена всех подпунктов меню в цикле, поэтому приходится вызывать макрос столько раз, сколько есть пунктов меню
+; Внимание, если количество пунктов в меню превышает 8, просто добавьте еще
+						local	d=1
 ;-----------------------------------------------------------------------------------------------------------
 ; Подпункты 1 пункта меню
 ;-----------------------------------------------------------------------------------------------------------
-				if		d<NUM_OF_MAIN_MENU_PUNKTS+1
-				local	a=1	
-				while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
-					Menu_#v(d)_Submenu_#v(a)
-						mod_PCL	symbol_pointer
-						dt		MENU_SUBMENU_NAME_1_#v(a),0
-					a+=1
-				endw
-					d+=1
-				endif
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_1_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
 ;-----------------------------------------------------------------------------------------------------------
 ; Подпункты 2 пункта меню
 ;-----------------------------------------------------------------------------------------------------------
-				if		d<NUM_OF_MAIN_MENU_PUNKTS+1
-				local	a=1	
-				while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
-					Menu_#v(d)_Submenu_#v(a)
-						mod_PCL	symbol_pointer
-						dt		MENU_SUBMENU_NAME_2_#v(a),0
-					a+=1
-				endw
-					d+=1
-				endif
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_2_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
 ;-----------------------------------------------------------------------------------------------------------
 ; Подпункты 3 пункта меню
 ;-----------------------------------------------------------------------------------------------------------
-				if		d<NUM_OF_MAIN_MENU_PUNKTS+1
-				local	a=1	
-				while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
-					Menu_#v(d)_Submenu_#v(a)
-						mod_PCL	symbol_pointer
-						dt		MENU_SUBMENU_NAME_3_#v(a),0
-					a+=1
-				endw
-					d+=1
-				endif
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_3_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
 ;-----------------------------------------------------------------------------------------------------------
 ; Подпункты 4 пункта меню
 ;-----------------------------------------------------------------------------------------------------------
-				if		d<NUM_OF_MAIN_MENU_PUNKTS+1
-				local	a=1	
-				while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
-					Menu_#v(d)_Submenu_#v(a)
-						mod_PCL	symbol_pointer
-						dt		MENU_SUBMENU_NAME_4_#v(a),0
-					a+=1
-				endw
-					d+=1
-				endif
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_4_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
 ;-----------------------------------------------------------------------------------------------------------
 ; Подпункты 5 пункта меню
 ;-----------------------------------------------------------------------------------------------------------
-				if		d<NUM_OF_MAIN_MENU_PUNKTS+1
-				local	a=1	
-				while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
-					Menu_#v(d)_Submenu_#v(a)
-						mod_PCL	symbol_pointer
-						dt		MENU_SUBMENU_NAME_5_#v(a),0
-					a+=1
-				endw
-					d+=1
-				endif
-
-			endm
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_5_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
+;-----------------------------------------------------------------------------------------------------------
+; Подпункты 6 пункта меню
+;-----------------------------------------------------------------------------------------------------------
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_5_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
+;-----------------------------------------------------------------------------------------------------------
+; Подпункты 7 пункта меню
+;-----------------------------------------------------------------------------------------------------------
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_5_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
+;-----------------------------------------------------------------------------------------------------------
+; Подпункты 8 пункта меню
+;-----------------------------------------------------------------------------------------------------------
+						if		d<NUM_OF_MAIN_MENU_PUNKTS+1
+							local	a=1	
+							while	a<NUM_OF_SUBMENU_PUNKTS_#v(d)+1
+								Menu_#v(d)_Submenu_#v(a)
+								mod_PCL	symbol_pointer
+								dt		MENU_SUBMENU_NAME_5_#v(a),0
+								a+=1
+							endw
+							d+=1
+						endif
+;-----------------------------------------------------------------------------------------------------------
+						endm
 ;================================================================================================================
 if	USE_MENU_ACTION	
-
 goto_Menu_action	macro		; Макрос, создает переходы на действия всех пунктов меню (если включена соответствующая опция)
 					mod_PCL	index_menu				
 					local	a=1
-				while	a<NUM_OF_MAIN_MENU_PUNKTS+1
-					goto	Menu_#v(a)_action
-					a+=1
-				endw
+					while	a<NUM_OF_MAIN_MENU_PUNKTS+1
+						goto	Menu_#v(a)_action
+						a+=1
+					endw
 					endm
-
 endif
